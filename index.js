60fcded424c756bfd29d012e758b198d5a0e0e6a
Mike <korostelevm@gmail.com>
2023-10-05 08:59:16 -0400

    adsf

---
+++ index.js
@@ -0,0 +1,27 @@
+let t = Date.now();
+const express = require('express')
+const app = express()
+const router = express.Router()
+
+router.get('*', async (req, res)=>{
+    console.log(req.path)
+    console.log(req.headers)
+    console.log("Just got a request!")
+    console.log(req.query)
+    console.log(req.url)
+    return res.json({
+        message: 'ok',
+        h: req.headers,
+        q: req.query,
+        route: req.url
+    })
+})
+
+app.use(router)
+
+
+app.listen(3001, ()=>{
+    console.log('Listening on port 3001')
+    console.log(`Startup time: ${Date.now() - t}ms`)
+    process.exit(0);
+})
